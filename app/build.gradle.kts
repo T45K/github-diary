aef5d7de67f83105457a40e813eb85a926771d38
T45K <tasktas9@gmail.com>
2025-12-20 23:53:55 +0900

    Update dependencies: Kotlin to 2.3.0, Ktor to 3.3.3, add Arrow Core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.3.0-RC3"
+    kotlin("jvm") version "2.3.0"
     id("org.jetbrains.compose") version "1.10.0-rc02"
-    kotlin("plugin.serialization") version "2.3.0-RC3"
+    kotlin("plugin.serialization") version "2.3.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0-RC2"
 }
 
@@ -21,19 +21,21 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
 
-    implementation("io.ktor:ktor-client-core:2.3.12")
-    implementation("io.ktor:ktor-client-cio:2.3.12")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
-    implementation("io.ktor:ktor-client-logging:2.3.12")
-
+    implementation("io.ktor:ktor-client-core:3.3.3")
+    implementation("io.ktor:ktor-client-cio:3.3.3")
+    implementation("io.ktor:ktor-client-content-negotiation:3.3.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.3.3")
+    implementation("io.ktor:ktor-client-logging:3.3.3")
+    
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains:markdown:0.7.3")
+
+    implementation("io.arrow-kt:arrow-core:2.2.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
-    testImplementation("io.ktor:ktor-client-mock:2.3.12")
+    testImplementation("io.ktor:ktor-client-mock:3.3.3")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
 
