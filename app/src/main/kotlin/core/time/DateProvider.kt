d6e0b409cb385dcc9344bb46b764a1f0e6eaf3c3
T45K <tasktas9@gmail.com>
2026-01-11 19:09:25 +0900

    Add currentYearMonth method to DateProvider

--- app/src/main/kotlin/core/time/DateProvider.kt
+++ app/src/main/kotlin/core/time/DateProvider.kt
@@ -1,12 +1,13 @@
 package core.time
 
 import kotlin.time.Clock
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.YearMonth
 import kotlinx.datetime.todayIn
+import kotlinx.datetime.yearMonth
 
 class DateProvider(
     private val clock: Clock = Clock.System,
 ) {
-    fun today(): LocalDate = clock.todayIn(TimeZone.of("Asia/Tokyo"))
+    fun currentYearMonth(): YearMonth = clock.todayIn(TimeZone.of("Asia/Tokyo")).yearMonth
 }
